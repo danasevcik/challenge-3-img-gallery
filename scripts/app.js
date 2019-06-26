6c0e27c47db2e6636c7512cf2667536717638711
github username <danasevcik@hotmail.com>
2019-06-26 11:03:17 -0400

    fetch flickr photos

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 window.addEventListener('DOMContentLoaded', (e) => {
   console.log('here');
+  let imgContainer = document.querySelector('#img-container')
+  console.log(imgContainer);
+  console.log(APIKEY);
+  fetch(`https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${APIKEY}&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1`)
+  .then(res => res.json())
+  .then(data => console.log(data))
 })
